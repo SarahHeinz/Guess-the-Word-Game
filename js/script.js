dce3cdeabcfc6ada19099b1e5cab79fd1465c76b
Sarah Heinz. <124076207+SarahHeinz@users.noreply.github.com>
2023-11-21 19:24:58 +0100

    Display Word and guessed Letters

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const guessedLettersElement = document.querySelector (".guessed-letters");
-const guessButton = document.querySelector (".guess");
-const remainGuess = document.querySelector (".remaining");
-const remainGuessSpan = document.querySelector (".remaining span");
-const message = document.querySelector (".message");
-const playAgain = document.querySelector (".play-again");
-const wordProgress = document.querySelector (".word-in-progress");
-const letterInput = document.querySelector (".letter");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const remainGuess = document.querySelector(".remaining");
+const remainGuessSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+const wordProgress = document.querySelector(".word-in-progress");
+const letterInput = document.querySelector(".letter");
 
 const word = "magnolia";
 const guessedLetters = [];
@@ -18,12 +18,12 @@ const placeholder = function (word) {
     for (const letter of word) {
         console.log(letter);
         placeholderLetters.push("●");
-    } 
+    }
     wordProgress.innerText = placeholderLetters.join("");
 };
-placeholder (word);
+placeholder(word);
 
-guessButton.addEventListener ("click", function(e) {
+guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     // Empty message paragraph
     message.innerText = "";
@@ -32,8 +32,8 @@ guessButton.addEventListener ("click", function(e) {
     if (goodGuess) {
         // We've got a letter! Let's guess!
         makeGuess(guess);
-      }
-      letterInput.value = "";
+    }
+    letterInput.value = "";
 });
 
 // Validate Player Input
@@ -43,10 +43,10 @@ const validateInput = function (input) {
     if (input.length === 0) {
         message.innerText = "Please enter a letter!";
         // more then 1 letter?
-    } else if (input.length >1){
+    } else if (input.length > 1) {
         message.innerText = "Please enter only one letter.";
         // something else then a letter?
-    } else if (!input.match(acceptedLetter)){
+    } else if (!input.match(acceptedLetter)) {
         message.innerText = "Please enter a letter from a-z.";
         // one single letter!
     } else {
@@ -60,7 +60,44 @@ const makeGuess = function (guess) {
     if (guessedLetters.includes(guess)) {
         message.innerText = "You already have guessed this letter.";
     } else {
-    guessedLetters.push(guess);
-    console.log(guessedLetters);    
-}
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordProgress(guessedLetters);
+    }
+};
+
+// Create a Function to Show the Guessed Letter
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+// Create a Function to Update the Word in Progress
+const updateWordProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    console.log(wordArray);
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    wordProgress.innerText = revealWord.join("");
+    checkWin();
+};
+
+// Create a Function to Check If the Player Won
+const checkWin = function () {
+    if (word.toUpperCase() === wordProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML= `<p class="highlight">You guessed the word correct! Congrats!</p>`;
+    }
 };
